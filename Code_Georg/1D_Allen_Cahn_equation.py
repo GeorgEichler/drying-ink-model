bf4824b803f21fe48320c4e1d1cab1ff0faba510
Georg Eichler <georg_eichler@web.de>
2025-02-05 18:00:59 +0000

    Solve PDE using BDF method of the solve_ivp function

--- Code_Georg/1D_Allen_Cahn_equation.py
+++ Code_Georg/1D_Allen_Cahn_equation.py
@@ -15,3 +15,25 @@ def fun(t, y, mu, h):
     dydt = d2yd2x + y - y**3 + mu
 
     return dydt
+
+L = 1   #length of domain [0,L]
+N = 64  #number of grid points
+h = L/N #grid spacing
+
+x_grid = np.linspace(start = h, stop = L - h, num = N)
+phi0 = 0.2*np.ones(N)
+
+mu = 0 #moisture parameter
+t_span = [0, 1]
+t_eval = np.linspace(0, 1, 101)
+
+solution = solve_ivp(fun, t_span = t_span, y0 = phi0, method = "BDF", t_eval = t_eval, args = (mu,h))
+
+for t in range(0, 101, 20):
+    plt.plot(x_grid, solution.y[:, t], label = f't = {t}')
+
+plt.xlabel('x')
+plt.ylabel(r'$\phi$')
+plt.title("Allen-Cahn evolution")
+plt.legend()
+plt.show()
