a3f22a90f2336d254da7f1e0754856ef830b9d0f
Georg Eichler <georg_eichler@web.de>
2025-02-05 17:37:17 +0000

    Add RHS of equation using periodic boundary conditions and central second order discretisation

--- Code_Georg/1D_Allen_Cahn_equation.py
+++ Code_Georg/1D_Allen_Cahn_equation.py
@@ -1,4 +1,17 @@
 '''
-Numerical analysis of nondimensionalised Allen-Cahn equation
+Numerical analysis of nondimensionalised Allen-Cahn equation using periodic bounddary conditions
 partial phi/partial t = partial^2 phi/partial x^2  + phi - phi^3 + mu
 '''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+def fun(t, y, mu, h):
+    ye = np.roll(y, 1)  #shift y to right
+    yw = np.roll(y, -1) #shift y to left
+
+    d2yd2x = (yw - 2*y + ye)/h**2
+    dydt = d2yd2x + y - y**3 + mu
+
+    return dydt
