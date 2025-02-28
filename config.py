8e074a8c717a65822b2c4c51382f18b6cc09265c
GeorgEichler <georg_eichler@web.de>
2025-02-28 17:11:48 +0000

    Change variable names

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import fenics as fe
 import numpy as np
 
 class Config:
-    def __init__(self, domain_size = (100,100), domain_length = 25, num_steps = 50, time_interval = 10,
+    def __init__(self, grid = (100,100), domain_length = 25, num_steps = 50, time_interval = 10,
                  mu = 0.0, eps = -1, M = 0.2, sigma = 0.03, constant_phi = 0, constant_n = 0):
 
-        self.domain = domain_size
+        self.grid = grid
         self.L = domain_length
         self.num_steps = num_steps
         self.T = time_interval
@@ -16,20 +16,20 @@ class Config:
         self.c_phi = constant_phi
         self.c_n = constant_n
 
-        self.nx, self.ny = domain_size
+        self.nx, self.ny = grid
         self.mesh = fe.RectangleMesh(fe.Point(0,0), fe.Point(self.L, self.L), self.nx, self.ny)
         self.V = fe.FunctionSpace(self.mesh, "Lagrange", 1)
 
         self.phi_options = {
             "constant": fe.Constant(self.c_phi),
             "sine_checkerboard": fe.Expression("sin(x[0])*sin(x[1])", degree=2),
-            "gaussian": fe.Expression("0.3*exp((-pow(x[0]-L/2, 2) -pow(x[1]-L/2, 2))/0.1)", L=self.L, degree=2),
+            "gaussian": fe.Expression("2*exp((-pow(x[0]-L/2, 2) -pow(x[1]-L/2, 2))/10) - 1", L=self.L, degree=2),
             "random": self.random()
         }
 
         self.n_options = {
             "constant": fe.Constant(self.c_n),
-            "gaussian": fe.Expression("0.3*exp((-pow(x[0]-L/2, 2) -pow(x[1]-L/2, 2))/0.1)", L=self.L, degree=2),
+            "gaussian": fe.Expression("exp((-pow(x[0]-L/2, 2) -pow(x[1]-L/2, 2))/10)", L=self.L, degree=2),
             "cross_gaussian": fe.Expression("0.1 * exp(-(pow(x[0] - L/2, 2) + pow(x[1] - L/2, 2))) * (exp(-pow(x[0] + x[1] - L, 2)/(2*0.1)) + exp(-pow(x[0] - x[1], 2)/(2*0.1)))",
                                     L=self.L, degree=2),
             "sine_checkerboard": fe.Expression("sin(x[0])*sin(x[1])", degree=2),
