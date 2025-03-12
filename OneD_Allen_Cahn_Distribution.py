e6e7195982f180aa025c156a9c257a2f5c17b73b
GeorgEichler <georg_eichler@web.de>
2025-03-12 01:27:55 +0000

    Add initial conditions to one dimensional model

--- OneD_Allen_Cahn_Distribution.py
+++ OneD_Allen_Cahn_Distribution.py
@@ -1,13 +1,23 @@
-import fenics
+import ufl
 from dolfin import *
 import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rcParams.update({
+            "axes.titlesize": 18,
+            "axes.labelsize": 16,
+            "xtick.labelsize": 14,
+            "ytick.labelsize": 14,
+            "legend.fontsize": 14,
+            "figure.dpi": 100 #change resolution, standard is 100
+        })
 
 # Parameters
 T = 10              # Time intervall length
 num_steps = 50      # number of time steps
 dt = T / num_steps  # time step size
 mu = 0.0            # moisture parameter
-L = 10              # domain length [0,L]
+L = 25              # domain length [0,L]
 eps = 1e-1          # how much the ink likes to be in the solvent
 alpha = 0.2*0.03    # product of evaporation coefficient (M) and surface tension (sigma)
 
@@ -16,18 +26,28 @@ nx = 1000
 mesh = IntervalMesh(nx, 0, L)
 V = FunctionSpace(mesh, "Lagrange", 1)
 
+def random(nx):
+    random_values = np.random.uniform(-0.1, 0.1, (nx+1))
+    phi_init = Function(V)
+    phi_init.vector()[:] = random_values.flatten()
+    interpolate(phi_init, V)
+    return phi_init
+
 # initial conditions
 #phi_k = interpolate(Expression("0.2*tanh(x[0]-5)", degree=2), V)
-phi_k = interpolate(Expression("x[0] < L/2 ? 0.2 : 0", L = L, degree=1),V)
+phi_k = interpolate(Expression("x[0] < L/2 ? -0.1 : 0.1", L = L, degree=1),V)
+#phi_k = interpolate(Expression("2*exp(-pow(x[0]-L/2, 2)/100)-1", L=L, degree=2), V)
+#phi_k = random(nx)
 phi_0 = phi_k.copy(deepcopy=True)
 
 #n_k = interpolate(Constant(1/L), V)
 #n_k = interpolate(Expression("x[0] < L/2 ? 0.2 : 0", L=L, degree=1), V)
-n_k = interpolate(Expression("0.1*exp(-pow(x[0]-L/2, 2)/0.01)", L=L, degree=2), V)
+n_k = interpolate(Constant(0), V)
+#n_k = interpolate(Expression("1*exp(-pow(x[0]-L/2, 2)/100)", L=L, degree=2), V)
 n_0 = n_k.copy(deepcopy=True)
 
-def D(x):
-    return (1 + x)**3
+def D(phi):
+    return 0.5*(1 + ufl.tanh(10*phi))
 
 # weak formulation of problem
 phi = Function(V)
@@ -62,7 +82,7 @@ for i in range(num_steps):
 plt.rcParams.update({'font.size': 18})
 x = mesh.coordinates().flatten()
 
-plotted_timesteps = [1, 50]
+plotted_timesteps = [num_steps //2, num_steps]
 
 plt.subplots(1, 2, figsize = (15,9))
 
@@ -71,7 +91,7 @@ plt.plot(x, phi_0.compute_vertex_values(), label="Initial condition")
 for j in plotted_timesteps:
     plt.plot(x, iterates_phi[j].compute_vertex_values(), label=f"t={j*dt}")
 
-plt.xlim(0,10)
+plt.xlim(0,L)
 plt.ylim(-1.1, 1.1)
 plt.xlabel("x")
 plt.ylabel("$\phi$")
@@ -82,7 +102,7 @@ plt.subplot(1, 2, 2)
 plt.plot(x, n_0.compute_vertex_values(), label="Initial condition")
 for j in plotted_timesteps:
     plt.plot(x, iterates_n[j].compute_vertex_values(), label=f"t={j*dt}")
-plt.xlim(0, 10)
+plt.xlim(0, L)
 plt.xlabel("x")
 plt.ylabel("n")
 plt.title("Solution of 1D Allen-Cahn\n equation (distribution)")
